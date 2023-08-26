b4c2e9ee95acb684cc94556801dac31b01f28ca9
Emilijan Koteski <emilijankoteski@pm.me>
2023-08-26 10:26:13 +0200

    added swiper library

---
+++ src/shared/components/card-slider/CardSlider.tsx
@@ -0,0 +1,34 @@
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import './card-slider.scss';
+
+function CardSlider() {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={32}
+      loop={true}
+      pagination={{
+        clickable: true
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className='card-slider'
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default CardSlider;
